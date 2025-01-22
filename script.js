370c77323e9bb386ba283b0c9388cc782630f0f6
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-22 18:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+const listContainer = document.getElementById("infi-list"); 
+let itemCount = 10; 
+let currentItemIndex = 1; 
 
+function createListItem(index) {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Item ${index}`;
+    return listItem;
+}
+
+function renderItems(count) {
+    for (let i = 0; i < count; i++) {
+        const listItem = createListItem(currentItemIndex++);
+        listContainer.appendChild(listItem);
+    }
+}
+
+listContainer.addEventListener("scroll", () => {
+        renderItems(2);
+});
+
+renderItems(itemCount);
